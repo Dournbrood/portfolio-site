de10f0463667db4675eb9564d0bfe31c19e25741
Dournbrood <tanner1220002@gmail.com>
2020-06-11 14:19:42 -0400

    fixes image sizing

--- src/components/About.js
+++ src/components/About.js
@@ -18,10 +18,9 @@ const About = () => {
           I am an aspiring full-stack developer from rural Pennsylvania. I enjoy
           playing video games, favoring VR titles like Beat Saber or cellular
           automation games like Factorio and Satisfactory. At work, I
-          particularly enjoy mentoring others, reviewing code, and have great
-          experience with the AGILE workflow. Learning new things is
-          first-nature to me, and I am more than happy to pick up new
-          frameworks, utilities and libraries.
+          particularly enjoy mentoring others, reviewing code and working with
+          the AGILE workflow. Learning new things is first-nature to me and I am
+          more than happy to pick up new frameworks, utilities and libraries.
         </p>
       </div>
     </div>
